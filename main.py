2576d2549cfc4238760a68da2a83d7e6fbbbb837
Paladin of Ioun <16229400+iounpaladin@users.noreply.github.com>
2021-08-05 10:00:07 -0700

    cleaned up

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import numpy as np
-import scipy as sp
 import scipy.optimize
 import scipy.interpolate
-from scipy.optimize import LinearConstraint, NonlinearConstraint
-import numpy.polynomial.polynomial as poly
 import matplotlib.pyplot as plt
 
 
-def W(x):
+def W(x):  # implementation of Weierstrass function for specific constants
     a = 0.85
     b = 7
 
@@ -24,43 +21,27 @@ def complexity(f, e):
     """
 
     R = -scipy.optimize.minimize_scalar(lambda x: -np.abs(f(x)), bounds=[0, 1], method='bounded').fun
-    m = 0
-    M = 0.5
-    h = 0.25
+    # find the value with which we normalize delta
 
     space = np.linspace(0, 0.5, 5000)
-    for i in space:
+    for i in space:  # find minimal i such that delta(f, i) / R > e
         if i == 0: continue
         if delta(f, i) / R > e:
             return -np.log(i)
 
     return 1
-    # for i in range(100):  # steps of binary search
-    #     g = delta(f, h) / R
-    #     if g > e:  # lower h if delta > e
-    #         M = h
-    #     else:  # otherwise, raise h (because delta is increasing in h, this always works)
-    #         m = h
-
-        # h = (m + M) / 2
-    # return -np.log(h)
 
 
-def delta(f, h, plot=False):
-    if h > 0.5:
+def delta(f, h):
+    if h > 0.5:  # cannot create the x-space if h is too large
         return 0
 
-    x = np.linspace(0, 1, int(np.ceil(1 / h)))
-    y = np.array(list(map(f, x)))
+    x = np.linspace(0, 1, int(np.ceil(1 / h)))  # create space of samples
+    y = np.array(list(map(f, x)))  # apply function to the space
 
-    g = scipy.interpolate.interp1d(x, y)
+    g = scipy.interpolate.interp1d(x, y)  # spline interpolation
     res = -scipy.optimize.minimize_scalar(lambda q: -abs(g(q) - f(q)), bounds=[0, 1], method='bounded').fun
-
-    if plot:
-        f2 = scipy.interpolate.interp1d(x, y, kind='cubic')
-        plt.plot(x, y, 'o', x, g(x), '-', x, f2(x), '--', x, np.array(list(map(f, x))), '-')
-        plt.legend(['data', 'linear', 'cubic', 'original'], loc='best')
-        plt.show()
+    # find maximal error (negate the minimum)
 
     return res
 
@@ -69,9 +50,6 @@ def run_complexity(f):
     x = []
     y = []
     for e in [
-        # 2,
-        # 1.5,
-        # 1.75
         0.5,
         0.1,
         0.05,
@@ -79,23 +57,23 @@ def run_complexity(f):
         0.005,
         0.001
     ]:
-        x.append(np.log(e))
-        y.append(complexity(f, e))
+        x.append(np.log(e))  # for several values of epsilon, record log(epsilon)
+        y.append(complexity(f, e))  # and S_e(f)
 
-    coeff = np.polynomial.polynomial.polyfit(x, y, 1)
+    coeff = np.polynomial.polynomial.polyfit(x, y, 1)  # best-fit line
 
     return x, y, coeff
 
 
 if __name__ == "__main__":
     def fmt(a):
-        return f"{round(a[0], 4)} + {round(a[1], 4)} log ε"
+        return f"{round(a[0], 4)} + {round(a[1], 4)} log ε"  # formatting for graph title
 
 
     x = np.linspace(0, 1, num=200)
     y = W(x)
     plt.plot(x, y, '-')
-    plt.title('W')
+    plt.title('W')  # graph Weierstrass function (just for fun)
     plt.show()
     for func, name in [
         (lambda x: np.sin(2 * np.pi * x), 'sin'),
@@ -103,10 +81,8 @@ if __name__ == "__main__":
         (lambda x: x ** (1 / 2), 'sqrt'),
         (lambda x: x ** (1 / 3), 'cbrt'),
         (W, 'W'),
-        # (lambda x: np.sin(1 / (x + 0.0001)), 'sin(1/x)'),
-        # (lambda x: np.cos(1 / (x + 0.0001)), 'cos(1/x)'),
     ]:
-        x, y, coeff = run_complexity(func)
+        x, y, coeff = run_complexity(func)  # for each function, plot the recorded values and best fit line
 
         plt.plot(x, y, 'o', x, np.polynomial.polynomial.polyval(x, coeff))
         plt.title(name + ' ' + fmt(coeff))
